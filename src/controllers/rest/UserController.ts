310a7acda0abc205a58d0847fcc3278f18394516
MARIE Raphael <56415306+R-o-h-t@users.noreply.github.com>
2022-12-15 11:47:42 +0100

    fix

--- src/controllers/rest/UserController.ts
+++ src/controllers/rest/UserController.ts
@@ -7,13 +7,14 @@ import { UserModel } from "@tsed/prisma";
 import { Authorize } from "@tsed/passport";
 import { User } from "@prisma/client";
 
+@Security("jwt")
+@Authorize("jwt")
 @Controller("/user")
 @Scope(ProviderScope.SINGLETON)
 export class UserCtrl {
   @Inject()
   userService: UserService;
-  @Security("jwt")
-  @Authorize("jwt")
+
   @Post("/create")
   @Description("Create a new user")
   async create(@Req("user") user: User | undefined, @BodyParams() body: UserCreation): Promise<UserModel> {
